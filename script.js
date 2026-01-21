fd3826d00c2cc7086143c18c4a269b5d16bb29bc
mishmishel <misheltyurkova@gmail.com>
2026-01-21 15:45:22 +1100

    Initial Etch-a-Sketch logic implemented Logic for divs changing colour when user hovers over them.

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-let container = document.querySelector('#container');
+let container = document.querySelector('.container');
 
-function makeGrid() {
-    for (let i = 0; i < 16; i++) {
+function makeGrid(num) {
+    for (let i = 0; i < num; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
         for (let j = 0; j < 16; j++) {
-            const row = document.createElement("div");
-            container.appendChild(row);
+            const rowMember = document.createElement("div");
+            row.appendChild(rowMember);
         }
+        container.appendChild(row);
     }
 }
 
-makeGrid();
+makeGrid(16);
+
+const divs = document.querySelectorAll("div");
+divs.forEach((div) => {
+    div.addEventListener('mouseover', () => {
+        console.log('Event');
+        div.style.backgroundColor = 'white';
+    })
+})
